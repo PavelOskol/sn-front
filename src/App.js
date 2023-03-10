e2dcab7fc7eacb3113945245b78b2193953ba20b
PavelOskol <pashajag@mail.ru>
2023-03-10 19:08:17 +0300

    added nav components / routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import Header from "./components/Head/Head";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/Content/Profile/Profile";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Messages from "./components/Content/Messages/Messages";
+import News from "./components/Content/News/News";
+import Music from "./components/Content/Music/Music";
 
 
 function App() {
-  return (
-      <div className="App">
-          <Header />
-          <Sidebar />
-          <Profile />
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Sidebar/>
+
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/" element={<Profile/>}/>
+                    <Route path="/messages" element={<Messages/>}/>
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/music" element={<Music/>}/>
+                    {/*<Route path="/messages" element={<Messages/>}/>*/}
+                </Routes>
+
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export {App} ;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,10 +3,11 @@ import s from "./Sidebar.module.css"
 
 export default function Sidebar() {
     return <nav className={s.sidebar}>
-        <a href="src/components#profile">Profile</a>
-        <a href="src/components#messages">Messages</a>
-        <a href="src/components#news">News</a>
+        <a href="/profile">Profile</a>
+        <a href="/messages">Messages</a>
+        <a href="/news">News</a>
         <br/>
-        <a href="src/components#music">Music</a>
+        <a href="/music">Music</a>
+        {/*<a href="src/components#music">Options</a>*/}
     </nav>
 }
